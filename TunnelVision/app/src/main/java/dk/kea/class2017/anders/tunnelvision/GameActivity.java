83381f52753d24241a69cac9573fef969057fa0d
anders11 <anderslatif@gmail.com>
2017-03-10 02:29:19 +0100

    First init, created the start screen menu

---
+++ TunnelVision/app/src/main/java/dk/kea/class2017/anders/tunnelvision/GameActivity.java
@@ -0,0 +1,13 @@
+package dk.kea.class2017.anders.tunnelvision;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
